892f6dcf5715504fface7444e6d1dfd31ea55c69
bogfoot <bogfoot94@yahoo.com>
2023-03-03 14:28:51 +0100

    changes in ?

--- main.cpp
+++ main.cpp
@@ -2,18 +2,14 @@
 #include <iostream>
 
 int main() {
-  Vec2D a{0.1F, 0.2F};
-  a.Print();
-  Vec2D b{1.0F, 1.0F};
-  b.Print();
-  Vec2D c = a + b;
-  c.Print();
+  int size = 3;
+  MatrixNxN nxn{size};
+  for (int i{0}; i < size; i++) {
+    for (int j{0}; j < size; j++) {
+      nxn[i][j] = j - i + 1;
+    }
+  }
+  nxn.PrintNxN();
 
-  Vec3D a_3{0.1F, 0.2F, 0.3F};
-  a_3.Print();
-  Vec3D b_3{1.0F, 1.0F, 1.0F};
-  b_3.Print();
-  Vec3D c_3 = a_3 + b_3;
-  c_3.Print();
   return 0;
 }
